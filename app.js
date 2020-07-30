67c7719467b5e418d3a488fd0072b51ec04bedfc
Anthony McGrath <akm@anthonykyle.co.nz>
2020-07-31 11:10:07 +1200

    Initialise campgrounds

--- app.js
+++ app.js
@@ -17,8 +17,21 @@ app.set('view engine', 'ejs');
 //*************************************** */
 // Routes                                 */
 //*************************************** */
+// app.get('/', (req,res) => {});
+
+// Landing Page
 app.get('/', (req,res) => {
   res.render('landing');
+});
+
+// Campgrounds
+app.get('/campgrounds', (req,res) => {
+  let campgrounds = [
+    {name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1500332988905-1bf2a5733f63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
+    {name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1545252682-2d32e48111da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=958&q=80'},
+    {name: 'Mountain Goat\'s Rest', image: 'https://images.unsplash.com/photo-1590122401860-bfebd9aae845?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'}
+  ];
+  res.render('campgrounds');
 });
 
 //*************************************** */
