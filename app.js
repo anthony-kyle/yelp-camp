eab7908b50da2b88653626efe8832c3cb012cef9
Anthony McGrath <akm@anthonykyle.co.nz>
2020-07-31 10:58:39 +1200

    Initialise app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+//************************************************* */
+// Title:   Yelp Camp Application                   */
+// File:    app.js                                  */
+// Author:  Anthony McGrath                         */
+// Email:   akm@anthonykyle.co.nz                   */
+// Repo:    github.com/anthony-kyle/yelp-camp.git   */
+//************************************************* */
+
+//*************************************** */
+// Initialise Frameworks                  */
+//*************************************** */
+const express = require('express');
+const app     = express();
+
+
+
+
+//*************************************** */
+// Initialise Server                      */
+//*************************************** */
+app.listen(3000, function(){
+  console.log('Yelp Camp running on port 3000');
+})
