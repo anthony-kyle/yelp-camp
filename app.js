3b94c42b8411f036c2ef3cd77ed8c90d56bcf63c
Anthony McGrath <akm@anthonykyle.co.nz>
2020-07-31 13:12:01 +1200

    Add Navbar

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.set('view engine', 'ejs');
 let campgrounds = [
   {name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1500332988905-1bf2a5733f63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
   {name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1545252682-2d32e48111da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=958&q=80'},
+  {name: 'Mountain Goat\'s Rest', image: 'https://images.unsplash.com/photo-1590122401860-bfebd9aae845?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
+  {name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1500332988905-1bf2a5733f63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
+  {name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1545252682-2d32e48111da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=958&q=80'},
+  {name: 'Mountain Goat\'s Rest', image: 'https://images.unsplash.com/photo-1590122401860-bfebd9aae845?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
+  {name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1500332988905-1bf2a5733f63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'},
+  {name: 'Granite Hill', image: 'https://images.unsplash.com/photo-1545252682-2d32e48111da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=958&q=80'},
   {name: 'Mountain Goat\'s Rest', image: 'https://images.unsplash.com/photo-1590122401860-bfebd9aae845?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'}
 ];
 
